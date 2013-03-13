19010a2bc380f402ddc2adbbac615760ff438258
Ryo Utsunomiya <millionsnowliving@gmail.com>
2013-03-13 19:37:51 +0900

    fetch+ループではなくfetchAllに変更

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,20 +6,10 @@ try {
 
     $stt = $db->prepare('SELECT id, post_title FROM posts');//見出し用にタイトルを呼び出し
     $stt->execute();
-
-    $posts = array();
-
-    $i = 0;
-    while ($row = $stt->fetch(PDO::FETCH_ASSOC)) {
-        $posts[$i]['id'] = $row['id'];
-        $posts[$i]['post_title'] = $row['post_title'];
-        //$posts[$i]['post_content'] = $row['post_content'];
-        $i++;
-    }
+    $posts = $stt->fetchAll(PDO::FETCH_ASSOC);
 
     $stt = $db->prepare('SELECT * FROM categories');//カテゴリー呼び出し
     $stt->execute();
-
     $categories = $stt->fetchAll(PDO::FETCH_ASSOC);
 
 } catch (PDOExeption $e) {
