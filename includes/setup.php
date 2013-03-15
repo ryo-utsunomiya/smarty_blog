d4f80061e9d8c2449dfe46371cbb7262a06f4643
Ryo Utsunomiya <millionsnowliving@gmail.com>
2013-03-15 20:02:35 +0900

    エラー表示制御はphp.iniで行うように変更

--- includes/setup.php
+++ includes/setup.php
@@ -1,6 +1,5 @@
 <?php
-
-ini_set('display_errors', 1); // デバッグ用にエラー表示
+//namespace Smarty_Blog;
 
 // Smartyライブラリを読み込み
 require_once('/usr/local/lib/Smarty-3.1.13/libs/Smarty.class.php');
@@ -28,6 +27,6 @@ class Smarty_Blog extends Smarty
         // 表示するテンプレートのデフォルト値を設定（デフォルト：.phpファイルと同名の.tplファイルを呼び出し）
         $this->tpl = str_replace('.php', '.tpl', $_SERVER['SCRIPT_NAME']);
         $this->tpl = str_replace('/smarty_blog/', '', $this->tpl);
-        $this->tpl = 'extends:layout.tpl|' . $this->tpl;
+        $this->tpl = 'extends:layout.tpl|' . $this->tpl;// レイアウト用のテンプレートを継承
     }
 }
