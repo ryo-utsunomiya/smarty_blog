3b28d2d415db3190f015553a9bdecbf0eb7d1904
Ryo Utsunomiya <millionsnowliving@gmail.com>
2013-03-21 14:34:39 +0900

    数字か検査するメソッドを追加

--- includes/Validator.php
+++ includes/Validator.php
@@ -4,6 +4,9 @@ class Validator
 {
     private $errors;
 
+    /**
+     * $_GET/POST/COOKIEのエンコーディングとヌルバイトをチェック
+     */
     public function __construct()
     {
         $errors = array();
@@ -17,15 +20,23 @@ class Validator
         $this->checkNull($_COOKIE);
     }
 
+    /**
+     * 与えられた値のエンコーディングがUTF-8になっているかチェック
+     * @param array $data 検査対象
+     */
     public function checkEncoding(array $data)
     {
         foreach ($data as $key => $value) {
-            if (!mb_check_encoding($value)) {
+            if (!mb_check_encoding($value, 'UTF-8')) {
                 $this->errors[] = "{$key}は不正な文字コードです。";
             }
         }
     }
 
+    /**
+     * 与えられた値がヌルバイトを含んでいないかチェック
+     * @param array $data 検査対象
+     */
     public function checkNull(array $data)
     {
         foreach ($data as $key => $value) {
@@ -33,6 +44,14 @@ class Validator
                 $this->errors[] = "{$key}は不正な文字を含んでいます。";
             }
         }
+    }
+
+    public function checkDigit($data)
+    {
+        if (!ctype_digit($data)) {
+            $this->errors[] = '{$data}は数字ではありません。';
+        }
+        return $data;
     }
 
     public function __invoke()
