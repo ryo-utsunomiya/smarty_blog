702de3a314b171e52387371509b53fdd710f1c5d
Ryo Utsunomiya <millionsnowliving@gmail.com>
2013-03-21 14:33:52 +0900

    カテゴリーIDを設定できるように変更

--- htdocs/admin/create_process.php
+++ htdocs/admin/create_process.php
@@ -5,15 +5,20 @@ if (empty($_POST)) {
     header('location:index.php');
 }
 
+$validator = new Validator(); // $_GET/POST/COOKIEの文字エンコーディングとヌルバイトを検査
+$category_id = $validator->checkDigit($_POST['category_id']);
+
 try {
 
     $db = getDb();
 
-    $query = 'INSERT INTO posts (post_title, post_content) VALUES (:post_title, :post_content)';
+    $query = 'INSERT INTO posts (post_title, post_content, category_id) VALUES (:post_title, :post_content, :category_id)';
     $stt = $db->prepare($query);
     $stt->bindValue(':post_title', $_POST['post_title']);
     $stt->bindValue(':post_content', $_POST['post_content']);
+    $stt->bindValue(':category_id', $category_id);
     $stt->execute();
+
     $db = null;
 
 } catch (PDOException $e) {
--- includes/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function debug($var) {
-    echo '<pre>';
-    var_dump($var);
-    echo '</pre>';
-}
